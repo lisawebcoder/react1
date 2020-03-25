e2ef808b34cb333491780634e8f98711ddd4807a
lisawebcoder <lisa@thefriendsnetwork.ca>
2020-03-25 10:15:20 -0400

    Updates

---
+++ precache-manifest.fd1352d69536fe04d9be53aaf07132f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1e501ec2ddc7453edf3dfbd2b919fe",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "d45b7388eb293c0ab2a8",
+    "url": "/react1/static/css/main.395142e4.chunk.css"
+  },
+  {
+    "revision": "7e161a8515bc1d815675",
+    "url": "/react1/static/js/2.12402e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react1/static/js/2.12402e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45b7388eb293c0ab2a8",
+    "url": "/react1/static/js/main.7a8fc473.chunk.js"
+  },
+  {
+    "revision": "260cc7a1e878e9390ddf",
+    "url": "/react1/static/js/runtime-main.86361f3a.js"
+  }
+]);
